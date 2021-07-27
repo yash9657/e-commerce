b114af13e61ac886b034449df2ca8458140f47d9
yash9657 <yashbhalgat9657@gmail.com>
2021-07-27 15:27:55 +0530

    First commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../../assets/Farout Logo.png'
+import useStyles from './styles';
+
+const Navbar = ({totalItems}) => {
+    const classes = useStyles();
+    const location = useLocation();
+
+    return (
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography component={Link} to="/" variant='h5' className={classes.title} color="inherit">
+                        <img src={logo} alt="Commerce.js" height="30px" className={classes.image}  />
+                        Farout Culture
+                    </Typography>
+                    <div className={classes.grow} />
+                    {location.pathname === '/' && (
+                    <div className={classes.button}>
+                        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={totalItems} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>)
+                    }
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar
